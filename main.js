d5fb78fc80ad4d1134b9f73a747ecfda5858e79a
Jayavel <jayavel1520@gmail.com>
2024-07-31 20:17:15 +0530

    version 1

---
+++ main.js
@@ -0,0 +1,26 @@
+const result = document.querySelector(".js-result");
+const checkPalindrome = () =>{
+    const inputElement = document.querySelector(".js-text-input");
+    let inputValue = inputElement.value.trim();
+
+    if(inputValue === ""){
+        alert("Please enter a string");
+        
+    } 
+    else{
+       const reversedtext = inputValue.split("").reverse().join("")
+       if (reversedtext === inputValue){
+        result.textContent = "The string is a palindrome";
+        result.classList.add("text-emerald-500")
+        result.classList.remove("hidden");
+       }
+       else{
+        result.textContent = "This string is not a palindrome";
+        result.classList.add("text-red-400")
+        result.classList.remove("hidden")
+       }
+       inputElement.value = ""
+    }
+}
+
+
